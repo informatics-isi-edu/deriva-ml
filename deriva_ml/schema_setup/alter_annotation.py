001661dbf23c94d8556cb99b697660c4a2f458d0
Zhiweiii <lizhiwei@usc.edu>
2024-04-11 23:12:47 -0700

    Added schema annotation alter and entry point.

--- deriva_ml/schema_setup/alter_annotation.py
+++ deriva_ml/schema_setup/alter_annotation.py
@@ -2,16 +2,19 @@ import sys
 import argparse
 from deriva.core import DerivaServer, get_credential, ErmrestCatalog
 from deriva.core.ermrest_model import builtin_types, Schema, Table, Column, ForeignKey
-from schema_annotation import generate_annotation
+from annotation_temp import generate_annotation
 
 
 def alter_table_annotation(catalog, schema_name: str, table_name: str, annotation: dict):
     model_root = catalog.getCatalogModel()
     table = model_root.table(schema_name, table_name)
-    table.alter(
-        annotations=annotation
-        )
+    table.alter(annotations=annotation)
 
+
+def alter_schema_annotation(catalog, schema_name: str, annotation: dict):
+    model_root = catalog.getCatalogModel()
+    schema = model_root.schemas[schema_name]
+    schema.alter(annotations=annotation)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,12 +25,12 @@ def main():
     credentials = get_credential(args.hostname)
     catalog = ErmrestCatalog('https', args.hostname, args.catalog_id, credentials)
 
-    annotations = generate_annotation(args.schema_name)
+    annotations = generate_annotation(args.schema_name, args.catalog_id)
     alter_table_annotation(catalog, args.schema_name, 'Workflow', annotations["workflow_annotation"])
     alter_table_annotation(catalog, args.schema_name, 'Execution', annotations["execution_annotation"])
     alter_table_annotation(catalog, args.schema_name, 'Execution_Metadata', annotations["execution_metadata_annotation"])
     alter_table_annotation(catalog, args.schema_name, 'Execution_Assets', annotations["execution_assets_annotation"])
-
+    alter_schema_annotation(catalog, args.schema_name, annotations["schema_annotation"])
 
 if __name__ == "__main__":
     sys.exit(main())
--- deriva_ml/schema_setup/annotation_temp.py
+++ deriva_ml/schema_setup/annotation_temp.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 
 
-def generate_annotation(schema: str) -> dict:
+def generate_annotation(catalog_id: str, schema: str) -> dict:
     workflow_annotation = {
         "tag:isrd.isi.edu,2016:visible-columns": {
             "*": [
@@ -173,18 +173,82 @@ def generate_annotation(schema: str) -> dict:
         }
     }
 
+    schema_annotation = {
+        "headTitle": "Catalog ML",
+        "navbarMenu": {
+            "newTab": false,
+            "children": [
+                {
+                    "name": "User Info",
+                    "children": [
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/public:ERMrest_Client",
+                            "name": "Users"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/public:ERMrest_Group",
+                            "name": "Groups"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/public:ERMrest_RID_Lease",
+                            "name": "ERMrest RID Lease"
+                        }
+                    ]
+                },
+                {
+                    "name": "FaceBase-ML",
+                    "children": [
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/{schema}:Workflow",
+                            "name": "Workflow"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/{schema}:Workflow_Type",
+                            "name": "Workflow Type"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/{schema}:Execution",
+                            "name": "Execution"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/{schema}:Execution_Metadata",
+                            "name": "Execution Metadata"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/{schema}:Execution_Metadata_Type",
+                            "name": "Execution Metadata Type"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/{schema}:Execution_Assets",
+                            "name": "Execution Assets"
+                        },
+                        {
+                            "url": f"/chaise/recordset/#{catalog_id}/{schema}:Execution_Asset_Type",
+                            "name": "Execution Asset Type"
+                        }
+                    ]
+                }
+            ]
+        },
+        "navbarBrandText": "ML Data Browser",
+        "systemColumnsDisplayEntry": ["RID"],
+        "systemColumnsDisplayCompact": ["RID"]
+    }
+
     return {"workflow_annotation": workflow_annotation,
             "execution_annotation": execution_annotation,
             "execution_assets_annotation": execution_assets_annotation,
-            "execution_metadata_annotation": execution_metadata_annotation
+            "execution_metadata_annotation": execution_metadata_annotation,
+            "schema_annotation": schema_annotation
             }
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--catalog_id', type=str, required=True)
     parser.add_argument('--schema_name', type=str, required=True)
     args = parser.parse_args()
-    return generate_annotation(args.schema_name)
+    return generate_annotation(args.catalog_id, args.schema_name)
 
 
 if __name__ == "__main__":
