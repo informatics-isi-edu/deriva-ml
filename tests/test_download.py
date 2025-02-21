db9cddd33c4919f500f72e6313a237f8d9dfbb03
Carl Kesselman <carl@isi.edu>
2025-02-21 10:13:00 -0800

    Working test for download.

--- tests/test_download.py
+++ tests/test_download.py
@@ -21,11 +21,23 @@ class TestDownload(TestDerivaML):
             )
         )
 
+        # The datasets in the bag should be all the datasets we started with.
+        self.assertEqual(
+            set([double_nested_dataset] + nested_datasets + datasets),
+            {k for k in bag.model.bag_rids.keys()},
+        )
+
+        # Children of top level bag should be in datasets variable
         self.assertEqual(
             set(nested_datasets), {ds.dataset_rid for ds in bag.list_dataset_children()}
         )
 
-        print(bag.list_dataset_children(recurse=True))
+        self.assertEqual(
+            set(nested_datasets + datasets),
+            {ds.dataset_rid for ds in bag.list_dataset_children(recurse=True)},
+        )
+
+        # Check to see if all of the files have been downloaded.
         files = [Path(r["Filename"]) for r in bag.get_table_as_dict("Image")]
         for f in files:
             self.assertTrue(f.exists())
