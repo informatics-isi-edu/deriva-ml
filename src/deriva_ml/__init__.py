de33dbdbd705a0dfb6568daaa74f30b07ed96a17
Carl Kesselman <carl@isi.edu>
2025-06-10 11:16:08 -0700

    Modified interface specification.

--- src/deriva_ml/__init__.py
+++ src/deriva_ml/__init__.py
@@ -27,3 +27,9 @@ try:
 except PackageNotFoundError:
     # package is not installed
     pass
+
+# Optional debug imports
+try:
+    from icecream import ic
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
