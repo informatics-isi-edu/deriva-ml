a321b74e1fc91f81c1d99289a284b5decfcb9cf8
Carl Kesselman <carl@isi.edu>
2025-06-16 13:40:29 -0700

    Addfiles test case.

--- tests/core/test_demo_catalog.py
+++ tests/core/test_demo_catalog.py
@@ -1,10 +1,14 @@
+from deriva_ml import MLVocab
+
+
 class TestVocabulary:
     def test_demo_vocabularies(self, test_ml_catalog_populated):
         ml_instance = test_ml_catalog_populated
         vocabs = ml_instance.model.find_vocabularies()
-        assert len([v for v in vocabs if v.schema.name == ml_instance.ml_schema]) == 6
+        assert len([v for v in vocabs if v.schema.name == ml_instance.ml_schema]) == len(MLVocab)
+        # SubjectHeath and ImageQuality
         assert len([v for v in vocabs if v.schema.name == ml_instance.domain_schema]) == 2
-        assert len(vocabs) == 8
+        assert len(vocabs) == len(MLVocab) + 2
         assert all(map(ml_instance.model.is_vocabulary, vocabs))
 
     def test_demo_terms(self, test_ml_catalog_populated):
@@ -17,25 +21,26 @@ class TestVocabulary:
         """Test dataset relationship management."""
         ml_instance = test_ml_catalog_populated
         datasets = ml_instance.find_datasets()
+        assert len(datasets) == 7
         double_nested_dataset = [d for d in datasets if "Complete" in d["Dataset_Type"]][0]["RID"]
         nested_datasets = [
             d["RID"] for e in ml_instance.list_dataset_members(double_nested_dataset).values() for d in e
         ]
         assert 2 == ml_instance._dataset_nesting_depth()
-        assert set(nested_datasets) == set(ml_instance.list_dataset_children(double_nested_dataset))
+        assert set(ml_instance.list_dataset_children(double_nested_dataset)) == set(nested_datasets)
         assert set([d["RID"] for d in datasets]) == {double_nested_dataset}.union(
             set(ml_instance.list_dataset_children(double_nested_dataset, recurse=True))
         )
 
         # Check parents and children.
-        assert 2 == len(ml_instance.list_dataset_children(nested_datasets[0]))
+        assert len(ml_instance.list_dataset_children(nested_datasets[0])) == 2
         assert double_nested_dataset == ml_instance.list_dataset_parents(nested_datasets[0])[0]
 
         # Verify relationship
         children = ml_instance.list_dataset_children(nested_datasets[0])
-
-        parents = ml_instance.get_dataset_parents(children[0])
-        assert any(p["RID"] == p["RID"] for p in parents)
+        assert len(children) == 2
+        parents = ml_instance.list_dataset_parents(children[0])
+        assert any([nested_datasets[0] == p for p in parents])
 
     def test_demo_datasets(self, test_ml_catalog_populated):
         ml_instance = test_ml_catalog_populated
--- tests/core/test_file.py
+++ tests/core/test_file.py
@@ -1,49 +1,95 @@
 import shutil
+import string
 from pathlib import Path
-from random import choice, randbytes
+from random import choice, choices
 
 import pytest
 
-from deriva_ml import ExecutionConfiguration, FileSpec, MLVocab
+from deriva_ml import DerivaMLInvalidTerm, ExecutionConfiguration, FileSpec, MLVocab
 
 
 class TestFile:
     @pytest.fixture(scope="class", autouse=True)
     def test_file_table_setup(self, test_ml_catalog, shared_tmp_path):
+        def random_string(length: int) -> str:
+            alphabet = string.ascii_letters + string.digits
+            return "".join(choices(alphabet, k=length))
+
         self.ml_instance = test_ml_catalog
-        self.ml_instance.add_term(MLVocab.workflow_type, "File Test Workflow",
-                                  description="Workflow for testing files")
-        self.workflow = self.ml_instance.create_workflow(name="Test Workflow",
-                                                         workflow_type="File Test Workflow")
+        self.ml_instance.add_term(MLVocab.workflow_type, "File Test Workflow", description="Workflow for testing files")
+        self.workflow = self.ml_instance.create_workflow(name="Test Workflow", workflow_type="File Test Workflow")
         self.execution = self.ml_instance.create_execution(
             ExecutionConfiguration(workflow=self.workflow, description="Test Execution")
         )
         self.test_dir = Path(shared_tmp_path) / "test_dir"
+        self.test_dir.mkdir(parents=True, exist_ok=True)
         d1 = self.test_dir / "d1"
+        d1.mkdir(parents=True, exist_ok=True)
         d2 = self.test_dir / "d2"
+        d2.mkdir(parents=True, exist_ok=True)
+
         self.file_count = 0
         for d in [self.test_dir, d1, d2]:
             for i in range(5):
-                with open(d / f"file{i}.{choice(['txt', 'jpg'])}", "w") as f:
-                    f.write(randbytes(256).decode())
-                    self.file_count += 1
+                self.file_count += 1
+                with open(d / f"file{i}.{choice(['txt', 'jpeg'])}", "w") as f:
+                    f.write(random_string(10))
         self.ml_instance.add_term(MLVocab.workflow_type, "File Test Workflow", description="Test workflow")
-        yield self.test_dir
+        yield self
         shutil.rmtree(self.test_dir)
         # Cleanup
 
-    def test_file_table(self):
-        with self.execution.execute() as exe:
-            filespecs = FileSpec.create_filespecs(self.test_dir, "Test Directory")
-            files = list(exe.add_files(filespecs))
-            assert len(files) == self.file_count
-            for r in files:
-                assert self.ml_instance.retrieve_rid(r)
+    def test_file_table_bad_type(self, test_file_table_setup):
+        ml_instance = test_file_table_setup
+        with pytest.raises(DerivaMLInvalidTerm):
+            filespec = [
+                FileSpec(
+                    url="tag://test_dir/file1.txt", description="Test file", md5="123", length=0, file_types=["foo"]
+                )
+            ]
+            ml_instance.execution.add_files(filespec)
+            filespec = [
+                FileSpec(
+                    url="tag://test_dir/file1.txt", description="Test file", md5="123", length=0, file_types=["foo"]
+                )
+            ]
+            ml_instance.execution.add_files(filespec)
 
-    def test_file_types(self, test_ml_catalog, test_files):
-            with self.execution.execute() as exe:
-                filespecs = FileSpec.create_filespecs(test_files, "Test Directory")
-                files = list(exe.add_files(filespecs))
-                assert len(files) == self.file_count
-                for r in files:
-                    assert self.ml_instance.read_file(r.rid)
+    def test_create_filespecs(self, test_file_table_setup):
+        ml_instance = test_file_table_setup.ml_instance
+        test_dir = test_file_table_setup.test_dir
+        execution = test_file_table_setup.execution
+
+        def use_extension(filename: Path) -> [str]:
+            return [filename.suffix.lstrip(".")]
+
+        with execution.execute() as exe:
+            filespecs = list(FileSpec.create_filespecs(test_dir, "Test Directory"))
+            assert len(filespecs) == test_file_table_setup.file_count
+            assert filespecs[0].file_types == ["File"]
+            filespecs = FileSpec.create_filespecs(test_dir, "Test Directory", file_types=["txt"])
+            assert all([set(filespec.file_types) == {"txt", "File"} for filespec in filespecs])
+            filespecs = FileSpec.create_filespecs(test_dir, "Test Directory", file_types=use_extension)
+            assert all([set(filespec.file_types) < {"txt", "jpeg", "File"} for filespec in filespecs])
+
+    def test_add_files(self, test_file_table_setup):
+        ml_instance = test_file_table_setup.ml_instance
+        test_dir = test_file_table_setup.test_dir
+        execution = test_file_table_setup.execution
+
+        def use_extension(filename: Path) -> [str]:
+            return [filename.suffix.lstrip(".")]
+
+        ml_instance.add_term(MLVocab.asset_type, "jpeg", description="A Image file")
+        ml_instance.add_term(MLVocab.asset_type, "txt", description="A Text file")
+        with execution.execute() as exe:
+            filespecs = FileSpec.create_filespecs(test_dir, "Test Directory", file_types=use_extension)
+
+            file_dataset = exe.add_files(filespecs)
+            assert file_dataset in [ds["RID"] for ds in ml_instance.find_datasets()]
+            ds = ml_instance.list_dataset_members(file_dataset)
+            assert len(ds["File"]) == 5
+            assert len(ds["Dataset"]) == 2
+            for subdir in ml_instance.list_dataset_children(file_dataset):
+                ds = ml_instance.list_dataset_members(subdir)
+                assert len(ds["File"]) == 5
