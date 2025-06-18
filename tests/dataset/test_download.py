e8a2df8a90d5141aeee792897f5371a2c0b228c7
Carl Kesselman <carl@isi.edu>
2025-06-18 09:04:19 -0700

    More cleanup on tests.

--- tests/dataset/test_download.py
+++ tests/dataset/test_download.py
@@ -9,9 +9,9 @@ class TestDownload:
         current_version = ml_instance.dataset_version(double_nested_dataset)
         subject_rid = ml_instance.list_dataset_members(datasets[0])["Subject"][0]["RID"]
         ml_instance.add_dataset_members(double_nested_dataset, [subject_rid])
-        new_version = ml_instance.dataset_version(double_nested_dataset)
+        # new_version = ml_instance.dataset_version(double_nested_dataset)
         bag = ml_instance.download_dataset_bag(DatasetSpec(rid=double_nested_dataset, version=current_version))
-        new_bag = ml_instance.download_dataset_bag(DatasetSpec(rid=double_nested_dataset, version=new_version))
+        # new_bag = ml_instance.download_dataset_bag(DatasetSpec(rid=double_nested_dataset, version=new_version))
 
         # The datasets in the bag should be all the datasets we started with.
         assert set([double_nested_dataset] + nested_datasets + datasets) == {k for k in bag.model.bag_rids.keys()}
@@ -27,13 +27,13 @@ class TestDownload:
             assert f.exists()
 
         def strip_times(members):
-            print(members)
             return {
                 element: [{k: v for k, v in m.items() if k not in ["RCT", "RMT"]} for m in mlist]
                 for element, mlist in members.items()
             }
 
         for ds in ml_instance.find_datasets():
+            print(ds)
             bag_members = bag.model.list_dataset_members(ds["RID"])
             catalog_members = ml_instance.list_dataset_members(ds["RID"])
             assert strip_times(bag_members) == strip_times(catalog_members)
